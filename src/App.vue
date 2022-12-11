1fbae53e8c8fbde36078bf43cb40bf8791d6d9f0
solomonayele <abc>
2022-12-10 19:57:04 -0600

    lab 10 sorry

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+ <NewStudentForm v-on:student-added="newStudentAdded"></NewStudentForm>
+  <StudentTable
+    v-bind:students="students"
+    v-on:student-present="studentArrivedOrLeft"
+    v-on:delete-student="studentDeleted">
+  </StudentTable>
+ <StudentMessage v-bind:student="mostRecentStudent"></StudentMessage>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NewStudentForm from './components/NewStudentForm.vue'
+import StudentTable from './components/StudentTable.vue'
+import StudentMessage from './components/StudentMessage.vue'
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      students: [],
+      mostRecentStudent: {}
+    }
+  },
   components: {
-    HelloWorld
+    StudentTable,
+    NewStudentForm,
+    StudentMessage
+  },
+  methods: {
+    updateStudents() {
+      this.$student_api.getAllStudents().then( students => {
+        this.students = students
+
+      })
+          .catch ( () => alert('Unable to fetch student list' ) )
+
+
+    },
+    newStudentAdded(student) {
+      this.$student_api.addStudent(student).then( () => {
+        this.updateStudents()
+      })
+
+          .catch ( err => {
+            alert('Error adding student. Star Id must be unique')
+          })
+    },
+    studentArrivedOrLeft(student, present) {
+      student.present = present // update present value
+      this.$student_api.updateStudents(student).then( () => {
+        this.mostRecentStudent = student
+        this.updateStudents()
+      })
+          .catch( () => alert( ' Unable to update student ' ))
+    },
+
+
+
   }
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
